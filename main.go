e4608b6761cd7e16341a6bde3e084fabccde390c
quduss <shuaib.qudus@yahoo.com>
2025-07-26 23:29:51 +0100

    Create main() to test config package

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("Error reading config: %v", err)
+	}
+	err = cfg.SetUser("qudus")
+	if err != nil {
+		log.Fatalf("Error setting user: %v", err)
+	}
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("Error reading config after update: %v", err)
+	}
+	fmt.Printf("Config contents:\n")
+	fmt.Printf("Database URL: %s\n", cfg.DbURL)
+	fmt.Printf("Current User: %s\n", cfg.CurrentUserName)
+}
