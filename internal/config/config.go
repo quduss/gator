e7b0d1c339f7e2b7ee2a6958ab1d278a9e3b4416
quduss <shuaib.qudus@yahoo.com>
2025-07-26 20:00:50 +0100

    Create a Read function that reads a config file in the user's home dir

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,29 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Config struct {
 	DbURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	configPath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return Config{}, err
+	}
+	var cfg Config
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		return Config{}, err
+	}
+	return cfg, nil
 }
