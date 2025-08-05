b2d9b3f8b06f950f5c281899dc89833a79df610a
quduss <shuaib.qudus@yahoo.com>
2025-08-05 07:55:10 +0100

    Write a function that fetches an RSS feed from the given URL and returns a parsed RSSFeed struct

--- rss.go
+++ rss.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"context"
+	"encoding/xml"
+	"html"
+	"io"
+	"net/http"
+)
+
 // RSSFeed represents the structure of an RSS feed
 type RSSFeed struct {
 	Channel struct {
@@ -16,4 +24,43 @@ type RSSItem struct {
 	Link        string `xml:"link"`
 	Description string `xml:"description"`
 	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	// Create a new HTTP request with context
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	// Set User-Agent header to identify our program
+	req.Header.Set("User-Agent", "gator")
+	// Create HTTP client and make the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	// Read the response body
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	// Parse the XML into our RSSFeed struct
+	var feed RSSFeed
+	err = xml.Unmarshal(data, &feed)
+	if err != nil {
+		return nil, err
+	}
+	// Unescape HTML entities in channel fields
+	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
+	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
+
+	// Unescape HTML entities in item fields
+	for i := range feed.Channel.Item {
+		feed.Channel.Item[i].Title = html.UnescapeString(feed.Channel.Item[i].Title)
+		feed.Channel.Item[i].Description = html.UnescapeString(feed.Channel.Item[i].Description)
+	}
+
+	return &feed, nil
 }
