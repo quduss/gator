47b79681f02d7fa9bc2ab3d18a70188930542179
quduss <shuaib.qudus@yahoo.com>
2025-08-05 07:33:15 +0100

    Create RSSFeed and RSSItem structs

---
+++ rss.go
@@ -0,0 +1,19 @@
+package main
+
+// RSSFeed represents the structure of an RSS feed
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+// RSSItem represents an individual item in an RSS feed
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
