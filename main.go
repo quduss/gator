d3227b967eed75ca9cce3f372c7d4c2a715f3756
quduss <shuaib.qudus@yahoo.com>
2025-08-06 09:02:33 +0100

    Register the Feeds Command

--- main.go
+++ main.go
@@ -263,6 +263,7 @@ func main() {
 	cmds.register("users", handlerUsers)
 	cmds.register("agg", handlerAgg)
 	cmds.register("addfeed", handlerAddFeed)
+	cmds.register("feeds", handlerFeeds)
 
 	args := os.Args
 	if len(args) < 2 {
